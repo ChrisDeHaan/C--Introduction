1107135316a20891950f37cbf65afd21f000817c
Chris DeHaan <christopherwdehaan@gmail.com>
2024-01-29 22:53:08 -0500

    first steps of C#

---
+++ CatWorx.BadgeMaker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+    static void Main(string[] args) // void signifies that the reutrn of this executable method will be void and static implies that the scope of this method is at the class level, not the object level, and can this be invoked without having to first create a new class instance.Hence the Main() method can be run as soon as the program runs.
+    // you could also intilize like static void Main() which has no parameters
+    {
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # C--Introduction
-C#-Introduction
+
+This repo is an introductory to C#. By the end of the lessons, I hope to be able to
+- Declare and use typed data
+- Print output and read user input from the console
+- Store collections of data in Arrays, ArrayLists, and Dictionaries
+- Declare and call methods
+- Use namespaces and statements to organize code and classes to implement object-oriented architectures
+- Compile and run a basic C# application in VS Code
