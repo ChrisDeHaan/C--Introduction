973fa67504b1f1a4969f6f98627fc8f5e2e8dc50
Chris DeHaan <christopherwdehaan@gmail.com>
2024-01-29 23:21:05 -0500

    1.3

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -2,20 +2,26 @@
 
 namespace CatWorx.BadgeMaker
 {
-  class Program
-  {
-    static void Main(string[] args) // void signifies that the reutrn of this executable method will be void and static implies that the scope of this method is at the class level, not the object level, and can this be invoked without having to first create a new class instance.Hence the Main() method can be run as soon as the program runs.
-    // you could also intilize like static void Main() which has no parameters
+    class Program
     {
-        float side1 = 3.14F; //you can use double here as well, but it is twice the program size. floats are good for ~6 decimals and double is used for ~15 decimals. end the value with F or D respectively
-        float area1 = side1 * side1;
-        Console.WriteLine("Area: {0}", area1);
-        Console.WriteLine("area is a: {0}", area1.GetType());
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, *
+            Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3
+            Console.WriteLine(1 + 2 * 3);     // order of operations
+            Console.WriteLine(10 / 3.0);      // int's and doubles
+            Console.WriteLine(10 / 3);        // int's 
+            Console.WriteLine("12" + "3");    // What happens here?
 
-        double side2 = 3.14D; //it is best practice to use doubles, as they have a much higher precision, going out to 15 decimal places
-        double area2 = side2 * side2;
-        Console.WriteLine("Area: {0}", area2);
-        Console.WriteLine("area is a: {0}", area2.GetType());
+            int num = 10;
+            num += 100;
+            Console.WriteLine(num);
+            num++;
+            Console.WriteLine(num);
+
+            bool isCold = true;
+            Console.WriteLine(isCold ? "drink" : "add ice");  // output: drink
+            Console.WriteLine(!isCold ? "drink" : "add ice");  // output: add ice
+        }
     }
-  }
 }
